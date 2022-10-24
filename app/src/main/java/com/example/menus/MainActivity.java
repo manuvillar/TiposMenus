f20c66c664b27fd07ce12a7fb8abeab165a51147
Manuel <mjvillar@iesoretania.es>
2022-10-24 09:16:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/menus/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.menus;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.idTextView);
+        registerForContextMenu(textView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =getMenuInflater();
+        inflater.inflate(R.menu.menu_opciones,menu);
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater =getMenuInflater();
+        inflater.inflate(R.menu.menu_opciones,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.idmenu1:
+                Toast.makeText(this, "Pulsada primera opción", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.idmenu2:
+                Toast.makeText(this, "Pulsada segunda opción", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.idmenu1:
+                Toast.makeText(this, "Pulsada primera opción", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.idmenu2:
+                Toast.makeText(this, "Pulsada segunda opción", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+}
